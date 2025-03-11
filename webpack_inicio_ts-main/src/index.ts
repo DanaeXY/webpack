db92a105543fad58b6fcb201464cc7a0fbf9705c
Israel <cursos.israelsp@gmail.com>
2025-03-11 16:03:56 +0100

    Actualización
    > Integramos a función de comunicación para solicitar datos o server
    
    Creamos unha nova carpeta, comunicacion, onde está dita función

--- webpack_inicio_ts-main/src/index.ts
+++ webpack_inicio_ts-main/src/index.ts
@@ -1,40 +1,15 @@
 import { CompraProducto } from "./clases/clases"
-// src/index.ts
-import './css/main.css'; // Asegúrate de que la ruta sea correcta
+import { comunicacionGET } from "./comunicacion/funcion.comunicacion";
 
-let instanciaCompraProducto = new CompraProducto(50,30,30)
-instanciaCompraProducto.compraProducto()
+import './css/main.css'; // Temos que integrar o CSS para que webpack o compile
 
-if(location.pathname == "/enviando-datos"){
-    console.log("estou de novo dentro ...")
 
+if(location.pathname == "/enviando-datos"){ 
     let reciboDatos = await fetch("/envio-datos-automaticamente");
     console.log("recibo datos get ",await reciboDatos.json())
-
+}
+if(location.pathname == "/logueo-basico"){
     const refBoton: HTMLButtonElement = document.querySelector("#envio");
-
-    refBoton.addEventListener("click",async ()=>{
-        type datosObxeto = {
-            dato1: string,
-            dato2: string,
-            dato3: number
-        }
-
-        let datosEnvio: datosObxeto = {
-            dato1: "mariano",
-            dato2: "pepito",
-            dato3: 5
-        }
-
-        let obxetoEnvio = {
-            method: 'post',
-            headers: {
-                "Content-type":"application/json"
-            },
-            body:JSON.stringify(datosEnvio)
-        }
-        let resposta = await fetch("/envio-datos-test",obxetoEnvio);
-        let respostaJson = await resposta.json()
-        console.log("isto ven do servidor",respostaJson)
-    })
+    console.log(refBoton)
+    refBoton.addEventListener("click",comunicacionGET)
 }
